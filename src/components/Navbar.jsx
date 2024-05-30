ac5b445899b54b73c0abfa6872f4f22b1d064449
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-05-30 13:04:28 +0200

    nav icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { router } from '../main'
+import React from "react"
+import { Link } from "react-router-dom"
+import { router } from "../main"
 
 export default function Navbar() {
-  // console.log('router', router.routes);
+  // console.log("router", router.routes);
 
   return (<nav>
     {router.routes.map(el=>{
       return <Link key={el.id} to={el.path}> 
-        <button>{ (el.name).toUpperCase() }</button> 
+        <img src={el.icon} alt={el.name}/>
       </Link>
     })}
   </nav>)
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './App.css'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./App.css"
+import "./index.css"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
 // STORE
 import {Provider} from "react-redux"
-import store from './datas/store'
+import store from "./datas/store"
 
 // PAGES
-import Home from './components/Home'
-import Ordinations from './components/Ordinations'
-import Cart from './components/Cart'
+import Home from "./components/Home"
+import Ordinations from "./components/Ordinations"
+import Cart from "./components/Cart"
 
 
 
 export const router= createBrowserRouter([
   {
     name: "home",
+    icon: "https://cdn.icon-icons.com/icons2/1660/PNG/512/3844470-home-house_110332.png",
     path: `/home`,
     element:<Home/>
   },
   {
     name: "ordinations",
+    icon:"https://cdn-icons-png.flaticon.com/256/857/857681.png",
     path: "/ordinations",
     element:<Ordinations/>
   },
   {
     name: "cart",
+    icon:"https://cdn.icon-icons.com/icons2/2645/PNG/512/cart_icon_160294.png",
     path: "/cart",
     element:<Cart/>
   },
 ])
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router}/>
