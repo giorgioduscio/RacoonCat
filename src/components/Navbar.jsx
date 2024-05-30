53bcab3ca4410228c0d2303260c0d02e2890ce4a
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-05-30 09:37:38 +0200

    navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { router } from '../main'
+
+export default function Navbar() {
+  console.log('router', router.routes);
+
+  return (<nav>
+    {router.routes.map(el=>{
+      return <Link key={el.id} to={`${el.path}`}> 
+        <button>{el.path}</button> 
+      </Link>
+    })}
+  </nav>)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './App.css'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// STORE
+import {Provider} from "react-redux"
+import store from './datas/store'
+
+// PAGES
+import Home from './components/Home'
+import Ordinations from './components/Ordinations'
+import Cart from './components/Cart'
+
+
+
+export const router= createBrowserRouter([
+  {
+    path:"/home",
+    element:<Home/>
+  },
+  {
+    path:"/ordinations",
+    element:<Ordinations/>
+  },
+  {
+    path:"/cart",
+    element:<Cart/>
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>,
 )
