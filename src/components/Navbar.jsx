87b3eb38496443c309d6916164da95efdef159b4
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-05-30 10:54:50 +0200

    navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { router } from '../main'
 
 export default function Navbar() {
-  console.log('router', router.routes);
+  // console.log('router', router.routes);
 
   return (<nav>
     {router.routes.map(el=>{
