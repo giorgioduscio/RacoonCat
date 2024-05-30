584bf9092a77ad3e4f9b668fc77f60416b3beb13
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-05-30 09:37:54 +0200

    pages

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Home() {
+  return (<>
+    <Navbar/>
+    <header>
+        <h3>Lorem ipsum dolor sit amet.</h3>
+        
+    </header>
+  </>)
+}
---
+++ src/components/Ordinations.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./Navbar"
+
+export default function Ordinations() {
+  return (<div className="ordinationsPage">
+    <Navbar/>
+
+    <header>
+      <h1>Sommario</h1>
+      <div className="">
+
+      </div>
+    </header>
+  </div>)
+}
