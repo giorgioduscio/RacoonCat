3534b278b8c4245db7cf2a2ca103c70bbce96b8d
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-06-01 12:46:51 +0200

    colore pulsanti navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { router } from "../main"
 
 export default function Navbar() {
-  // console.log("router", router.routes);
 
   return (<nav>
     {router.routes.map(el=>{
       return <Link key={el.id} to={el.path}> 
-        <img src={el.icon} alt={el.name}/>
+        <img src={el.icon} alt={el.name} className={el.name}/>
       </Link>
     })}
   </nav>)
